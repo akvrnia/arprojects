f2f944cf598e01ac435a923b88986cf14886a26d
akvrnia <akvrnia@gmail.com>
2025-02-14 16:03:00 +0700

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://akvrnia.github.io/arproject/',
+  base: 'https://akvrnia.github.io/arprojects/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
